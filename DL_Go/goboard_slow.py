c244f4825af6425e2394ca88485574fa5f5cab05
PhilosopheAM <11911421@mail.sustech.edu.cn>
2024-06-29 16:51:41 +0800

    Chapter 3.16

--- DL_Go/goboard_slow.py
+++ DL_Go/goboard_slow.py
@@ -1,5 +1,5 @@
 import copy
-from gotypes import Player
+from gotypes import Player, Point
 
 '''
 We following American Go Association(AGA)'s notation. At each round, a player should conduct a 'move'. A 'move' can be the following three actions:
@@ -55,7 +55,7 @@ class GoString():
         return GoString(
             self.color,
             merged_stones_set,
-            (self.liberties | go_string.liberties) - merged_stones_set
+            (self.liberties | go_string.liberties) - merged_stones_set ## Here do union first, then do difference.  
         )
     
     @property
@@ -68,3 +68,50 @@ class GoString():
                 self.stones == other.stones and \
                 self.liberties == other.liberties        
                 )
+    
+class Board():
+    def __init__(self, num_rows,num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = {} ## _grid is a dict, we use 'get' method latter to get GoString class instance stored inside.
+
+    def place_stone(self, player, point):
+        assert self.is_on_grid(point) ## Make sure the coordinate given is usable
+        assert self._grid.get(point) is None ## Make sure there is no stone in the place of the given coordinate of point
+        adjacent_same_color = []
+        adjacent_opposite_color = []
+        liberties = []
+        for neighbor_stone in point.neighbors():
+            if not self.is_on_grid(neighbor_stone):
+                continue ## We use keyword 'continue' to escape from this round of iteration. 
+            
+            ## Only those neighbors with reachable coordinates will go into following steps
+            neighbor_string = self._grid.get(neighbor_stone)
+            if neighbor_string is None:
+                liberties.append(neighbor_stone)
+            elif neighbor_string.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+            else: ## Here remains the only condition, the neighbor_string is not the same color as the player. We still need to check if the neighbor_string has already included in adjacent_opposite_color.
+                if neighbor_string not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_string) 
+        
+        new_string = GoString(player, [point], liberties)
+        ## Assemble the given GoString type instance with each of the adjacent GoString type
+
+
+
+    def is_on_grid(self,point):
+        return (1 <= point.row <= self.num_rows) and (1 <= point.col <= self.num_cols)
+
+    def get(self, point):
+        stone_string = self._grid.get(point)
+        if stone_string is None:
+            return None
+        return stone_string.color ## 'color' is Player class
+    
+    def get_go_string(self,point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string
